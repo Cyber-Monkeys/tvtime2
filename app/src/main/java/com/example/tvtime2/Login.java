3508726b3fd9f80c2dcbe4ae3eccc95a7c76c518
fidellim <lim.fidel@gmail.com>
2019-11-09 18:25:52 +0400

    create login page

--- app/src/main/java/com/example/tvtime2/Login.java
+++ app/src/main/java/com/example/tvtime2/Login.java
@@ -2,7 +2,9 @@ package com.example.tvtime2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void goToMainActivity(View view){
+        startActivity(new Intent(Login.this, MainActivity.class));
+    }
+
+//    public void goTo(View view){
+//        startActivity(new Intent(MainActivity.this, Login.class));
+//    }
 }
