6f386738eea29d74a32e633390c741508f046116
fidellim <lim.fidel@gmail.com>
2019-11-09 14:19:27 +0400

    add onclick functions change opacity of background image

--- app/src/main/java/com/example/tvtime2/MainActivity.java
+++ app/src/main/java/com/example/tvtime2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tvtime2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set background transparency to 90%
+        View backgroundimage = findViewById(R.id.background);
+        Drawable background = backgroundimage.getBackground();
+        background.setAlpha(230);
+    }
+
+
+    public void goToSignUp(View view){
+        startActivity(new Intent(MainActivity.this, Signup.class));
+    }
+
+    public void goToLogIn(View view){
+        startActivity(new Intent(MainActivity.this, Login.class));
     }
 }
