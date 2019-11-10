10d238574517fe90380b8afea517d281d788cfcf
fidellim <lim.fidel@gmail.com>
2019-11-10 20:43:06 +0400

    add recyclerview

--- app/src/main/java/com/example/tvtime2/Profile.java
+++ app/src/main/java/com/example/tvtime2/Profile.java
@@ -2,30 +2,37 @@ package com.example.tvtime2;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.lang.reflect.Field;
-
+import java.util.ArrayList;
 import maes.tech.intentanim.CustomIntent;
 
 public class Profile extends AppCompatActivity {
 
+
+    ArrayList<ProfileInfo> profileInfoArrayList = new ArrayList<ProfileInfo>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileInfoArrayList.add(new ProfileInfo("TV Time", "10 09 09", "months days hours"));
+        profileInfoArrayList.add(new ProfileInfo("EPISODES WATCHED", "17.3K", "46 in the last month"));
+        profileInfoArrayList.add(new ProfileInfo("MOVIE Time", "01 04 18", "months days hours"));
+        profileInfoArrayList.add(new ProfileInfo("MOVIES WATCHED", "455", ""));
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setNestedScrollingEnabled(false);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setAdapter(new HorizontalAdapter(profileInfoArrayList));
+        //recyclerView.setAdapter(new HorizontalAdapter(new String[]{"android","cdsds","dsdsd"}));
+
 
         BottomNavigationView bottomNavigationMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationMenu.clearAnimation();
