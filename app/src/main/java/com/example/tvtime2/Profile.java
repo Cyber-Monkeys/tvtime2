7489dcbc23403acebb5abb7e8bedc518123a8285
fidellim <lim.fidel@gmail.com>
2019-11-11 02:02:33 +0400

    update profile

--- app/src/main/java/com/example/tvtime2/Profile.java
+++ app/src/main/java/com/example/tvtime2/Profile.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.ArrayList;
@@ -15,24 +19,59 @@ import maes.tech.intentanim.CustomIntent;
 
 public class Profile extends AppCompatActivity {
 
-
+    //variables
+    TextView question_mark;
+    TextView bell;
+    TextView share;
+    TextView greater_than, greater_than2, greater_than3, greater_than4, greater_than5;
     ArrayList<ProfileInfo> profileInfoArrayList = new ArrayList<ProfileInfo>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        profileInfoArrayList.add(new ProfileInfo("TV Time", "10 09 09", "months days hours"));
+        question_mark = (TextView) findViewById(R.id.question_mark_text);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fa-solid-900.ttf"); //question_mark
+        question_mark.setTypeface(font);
+        question_mark.setText("\uf128");
+
+        bell = (TextView) findViewById(R.id.bell_text);
+        Typeface font2 = Typeface.createFromAsset(getAssets(), "fonts/fa-regular-400.ttf"); //bell
+        bell.setTypeface(font2);
+        bell.setText("\uf0f3");
+
+        share = (TextView) findViewById(R.id.share_text);
+        Typeface font3 = Typeface.createFromAsset(getAssets(), "fonts/fa-solid-900.ttf"); //share
+        share.setTypeface(font3);
+        share.setText("\uf14d");
+
+        greater_than = (TextView) findViewById(R.id.greater_than);
+        greater_than2 = (TextView) findViewById(R.id.greater_than2);
+        greater_than3 = (TextView) findViewById(R.id.greater_than3);
+        greater_than4 = (TextView) findViewById(R.id.greater_than4);
+        greater_than5 = (TextView) findViewById(R.id.greater_than5);
+        Typeface font4 = Typeface.createFromAsset(getAssets(), "fonts/fa-solid-900.ttf"); //greater than
+        greater_than.setTypeface(font4);
+        greater_than.setText("\uf531");
+        greater_than2.setTypeface(font4);
+        greater_than2.setText("\uf531");
+        greater_than3.setTypeface(font4);
+        greater_than3.setText("\uf531");
+        greater_than4.setTypeface(font4);
+        greater_than4.setText("\uf531");
+        greater_than5.setTypeface(font4);
+        greater_than5.setText("\uf531");
+
+        profileInfoArrayList.add(new ProfileInfo("TV Time", "10      09      09", "months            days               hours  "));
         profileInfoArrayList.add(new ProfileInfo("EPISODES WATCHED", "17.3K", "46 in the last month"));
-        profileInfoArrayList.add(new ProfileInfo("MOVIE Time", "01 04 18", "months days hours"));
+        profileInfoArrayList.add(new ProfileInfo("MOVIE Time", "01      04      18", "months            days               hours  "));
         profileInfoArrayList.add(new ProfileInfo("MOVIES WATCHED", "455", ""));
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setNestedScrollingEnabled(false);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(new HorizontalAdapter(profileInfoArrayList));
-        //recyclerView.setAdapter(new HorizontalAdapter(new String[]{"android","cdsds","dsdsd"}));
-
 
         BottomNavigationView bottomNavigationMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationMenu.clearAnimation();
@@ -64,5 +103,16 @@ public class Profile extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public void goToHelpPage(View view){
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://tvtime.zendesk.com/hc/en-us")));
+    }
+
+    public void goToShare(View view){
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        i.setType("text/plain");
+        startActivity(Intent.createChooser(i, "Share with..."));
     }
 }
