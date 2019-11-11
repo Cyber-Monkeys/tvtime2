bc216aeb4e7d77e0d6213d7740794be55320b50b
fidellim <lim.fidel@gmail.com>
2019-11-12 00:59:47 +0400

    add notificationInfo adapter

---
+++ app/src/main/java/com/example/tvtime2/NotificationInfoAdapter.java
@@ -0,0 +1,55 @@
+package com.example.tvtime2;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class NotificationInfoAdapter extends ArrayAdapter<NotificationInfo> {
+
+    //variables
+    private Context mContext;
+    int mResource;
+
+    public NotificationInfoAdapter(@NonNull Context context, int resource, @NonNull ArrayList<NotificationInfo> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+    //Attaching to listView
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        int photo = getItem(position).getImage();
+        String accomplishment = getItem(position).getAccomplishment();
+        String day = getItem(position).getDay();
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        if(convertView == null){
+            convertView = inflater.inflate(mResource, parent, false);
+        }
+
+        ImageView tvPhoto = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView tvAccomplishment = (TextView) convertView.findViewById(R.id.textView2);
+        TextView tvDay = (TextView) convertView.findViewById(R.id.textView3);
+
+        tvPhoto.setImageResource(photo);
+        tvAccomplishment.setText(accomplishment);
+        tvDay.setText(day);
+
+        return convertView;
+
+    }
+}
