c76b45548868710a050a750e089b91a085dd12dc
fidellim <lim.fidel@gmail.com>
2019-11-13 01:44:23 +0400

    add bottom bar

--- app/src/main/java/com/example/tvtime2/activity_shows_watch_list_unselected_page.java
+++ app/src/main/java/com/example/tvtime2/activity_shows_watch_list_unselected_page.java
@@ -1,11 +1,17 @@
 package com.example.tvtime2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import maes.tech.intentanim.CustomIntent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_shows_watch_list_unselected_page extends AppCompatActivity {
 
@@ -13,6 +19,37 @@ public class activity_shows_watch_list_unselected_page extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shows_watch_list_unselected_page);
+
+        BottomNavigationView bottomNavigationMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationMenu.clearAnimation();
+
+        Menu menu = bottomNavigationMenu.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_shows:
+                        Toast.makeText(activity_shows_watch_list_unselected_page.this, "Shows is clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_movies:
+                        startActivity(new Intent(activity_shows_watch_list_unselected_page.this, MoviesWatchList.class));
+                        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_discover:
+                        startActivity(new Intent(activity_shows_watch_list_unselected_page.this, Discover.class));
+                        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_profile:
+                        startActivity(new Intent(activity_shows_watch_list_unselected_page.this, Profile.class));
+                        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     public void goToMovies(View view){
@@ -28,11 +65,14 @@ public class activity_shows_watch_list_unselected_page extends AppCompatActivity
     public void goToProfile(View view){
         startActivity(new Intent(this, Profile.class));
         CustomIntent.customType(this, "fadein-to-fadeout");
+        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
     }
     public void menuPageSelect(View V) {
         startActivity(new Intent(this, ShowsWatchListDefaultPage.class));
+        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
     }
     public void goToUpcomingTab(View V) {
         startActivity(new Intent(this, UpcomingHomePage.class));
+        CustomIntent.customType(activity_shows_watch_list_unselected_page.this, "fadein-to-fadeout");
     }
 }
