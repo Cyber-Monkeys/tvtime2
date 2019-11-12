ae94116210f13dfd0c3a266ecc8b3c9dd2bf8dd4
fidellim <lim.fidel@gmail.com>
2019-11-13 01:20:40 +0400

    add animation

--- app/src/main/java/com/example/tvtime2/ShowsWatchListDefaultPage.java
+++ app/src/main/java/com/example/tvtime2/ShowsWatchListDefaultPage.java
@@ -1,10 +1,16 @@
 package com.example.tvtime2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import maes.tech.intentanim.CustomIntent;
 
@@ -14,6 +20,38 @@ public class ShowsWatchListDefaultPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shows_watch_list_default_page);
+
+        BottomNavigationView bottomNavigationMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationMenu.clearAnimation();
+
+        Menu menu = bottomNavigationMenu.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_shows:
+                        Toast.makeText(ShowsWatchListDefaultPage.this, "Shows is clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_movies:
+                        startActivity(new Intent(ShowsWatchListDefaultPage.this, MoviesWatchList.class));
+                        CustomIntent.customType(ShowsWatchListDefaultPage.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_discover:
+                        startActivity(new Intent(ShowsWatchListDefaultPage.this, Discover.class));
+                        CustomIntent.customType(ShowsWatchListDefaultPage.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_profile:
+                        startActivity(new Intent(ShowsWatchListDefaultPage.this, Profile.class));
+                        CustomIntent.customType(ShowsWatchListDefaultPage.this, "fadein-to-fadeout");
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 
     public void goToMovies(View view){
