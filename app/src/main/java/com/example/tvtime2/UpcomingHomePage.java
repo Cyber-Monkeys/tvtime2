d6bc9dd1ebb1287ed735f3decbefb6c7921ae595
fidellim <lim.fidel@gmail.com>
2019-11-13 01:44:59 +0400

    add bottom bar

--- app/src/main/java/com/example/tvtime2/UpcomingHomePage.java
+++ app/src/main/java/com/example/tvtime2/UpcomingHomePage.java
@@ -1,11 +1,17 @@
 package com.example.tvtime2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import maes.tech.intentanim.CustomIntent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UpcomingHomePage extends AppCompatActivity {
 
@@ -13,6 +19,37 @@ public class UpcomingHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upcoming_home_page);
+
+        BottomNavigationView bottomNavigationMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationMenu.clearAnimation();
+
+        Menu menu = bottomNavigationMenu.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_shows:
+                        Toast.makeText(UpcomingHomePage.this, "Show is clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_movies:
+                        startActivity(new Intent(UpcomingHomePage.this, MoviesWatchList.class));
+                        CustomIntent.customType(UpcomingHomePage.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_discover:
+                        startActivity(new Intent(UpcomingHomePage.this, Discover.class));
+                        CustomIntent.customType(UpcomingHomePage.this, "fadein-to-fadeout");
+                        break;
+                    case R.id.action_profile:
+                        startActivity(new Intent(UpcomingHomePage.this, Profile.class));
+                        CustomIntent.customType(UpcomingHomePage.this, "fadein-to-fadeout");
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     public void goToMovies(View view){
@@ -31,6 +68,7 @@ public class UpcomingHomePage extends AppCompatActivity {
     }
     public void menuPageUnselected(View V) {
         startActivity(new Intent(this, activity_shows_watch_list_unselected_page.class));
+        CustomIntent.customType(this, "fadein-to-fadeout");
     }
 
 }
