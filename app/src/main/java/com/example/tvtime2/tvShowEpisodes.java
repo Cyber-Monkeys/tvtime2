9067b8b6c4f29376c904e70bab9ae9b3d03511f6
Humaid Khan <humaidk2@gmail.com>
2019-11-13 22:29:41 +0400

    Create new tv show page Add cardview and top bar to tv show page Add new motion layout to tv show page

---
+++ app/src/main/java/com/example/tvtime2/tvShowEpisodes.java
@@ -0,0 +1,14 @@
+package com.example.tvtime2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tvShowEpisodes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_show_episodes);
+    }
+}
